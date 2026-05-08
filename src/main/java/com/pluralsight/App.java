10b0518281e5ad863afc6e624f93d8613fbca30c
ezrateshale <ezra.teshale12@gmail.com>
2026-05-08 08:19:27 -0500

    feat: Bonus Blackjack complete with Color Customization

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -15,6 +15,10 @@ public class App {
     static final String YELLOW = "\u001B[33m";
 
     public static void main(String[] args) {
+        blackjackGameDisplay();
+    }
+
+    private static void blackjackGameDisplay() {
         boolean isPlaying = true;
         while (isPlaying) {
             int numberOfPlayers = 0;
@@ -43,11 +47,7 @@ public class App {
                 players.add(new Player(playerName));
             }
             Player dealer = new Player("Dealer");
-//        Hand hand1 = new Hand();
-//        Hand hand2=new Hand();
-//        // deal 5 cards
-//        Hand player = new Hand();
-//        Hand dealer = new Hand();
+
             Deck deck = new Deck();
             deck.shuffle();
 
@@ -77,31 +77,6 @@ public class App {
                 System.out.println(GREEN + "Dealer wins! 🏆" + RESET);
             }
 
-//        int handValue = hand1.getValue();
-//        int handSecondValue = hand2.getValue();
-//        int handThirdValue = player.getValue();
-//        int dealerHandValue = dealer.getValue();
-//
-//        allHands.add(handValue);
-//        allHands.add(handSecondValue);
-//        allHands.add(handThirdValue);
-//        allHands.add(dealerHandValue);
-//        int closest = allHands.get(0);
-//        for(Player player : players) {
-//            allHands.add(player.getHandValue()); //
-//        }
-//
-//            // check if this value is closer to 21 than current closest
-//            // and not over 21 (bust)
-//            if(value <= 21 && Math.abs(21 - value) < Math.abs(21 - closest)) {
-//                closest = value;
-//            }
-
-//        System.out.println("Closest to 21: " + closest);
-//        System.out.println("This hand is worth: " + handValue);
-//        System.out.println("This hand is worth: " + handSecondValue);
-//        System.out.println("Player hand is worth: " + handThirdValue);
-//        System.out.println("Dealer hand is worth: " + dealerHandValue);
             boolean validAnswer = true;
             while (validAnswer) {
                 System.out.println("Do you wanna keep playing");
@@ -118,25 +93,7 @@ public class App {
                     System.out.println("Invalid input");
                 }
             }
-//        }
-//        for(int i = 0; i < 2; i++) {
-//            // get a card from the deck
-//            hand1.deal(deck.deal());
-//            hand2.deal(deck.deal());
-//            player.deal(deck.deal());
-//            dealer.deal(deck.deal());
-//        }
-//        handValue = hand1.getValue();
-//        handSecondValue = hand2.getValue();
-//        handThirdValue = player.getValue();
-//        dealerHandValue = dealer.getValue();
-//        System.out.println("This hand is worth: " + handValue);
-//        System.out.println("This hand is worth: " + handSecondValue);
-//        System.out.println("Player hand is worth: " + handThirdValue);
-//        System.out.println("Dealer hand is worth: " + dealerHandValue);
-//        if(player.getValue() == 21) {
-//            System.out.println("Blackjack! Player wins!");
-//        }
+
         }
     }
 
@@ -176,7 +133,6 @@ public class App {
             }
         }
 
-// dealer hits until 17
         System.out.println(YELLOW + "Dealer's turn..." + RESET);
         while (dealer.getHandValue() < 17) {
             System.out.println(YELLOW + "Dealer hits..." + RESET);
